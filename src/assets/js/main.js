d3067bce43c1005de5279e2e09b39d86ea926c86
yesoer <luisenglaender@web.de>
2022-05-01 21:04:04 +0200

    improve navigation responsiveness
    also rename sidebar* to navbar* and format index.html

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,12 @@
 $(function() {
-    // Sidebar toggle behavior
-    $('#sidebarCollapse').on('click', function() {
-      $('#sidebar, #content').toggleClass('active');
+    // navbar toggle behavior
+    $('#navbarCollapse').on('click', function() {
+      $('#navbar, #content').toggleClass('active');
     });
 });
+
+$(function() {
+  // from current url deduct which navitem should be active
+	var pathname = window.location.pathname;
+	$('.nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+})
