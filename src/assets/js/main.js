4a8bc58e7673c0183d4aa17d95fb49fd53606bd5
yesoer <luisenglaender@web.de>
2022-05-07 16:04:30 +0200

    implement table setup
    find the table body
    for each data entry add a table row and to said row, cells based on that entry
    setup edit function per row such that they store the entry to be edited in localStorage

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -37,6 +37,50 @@ function todoEditSetup() {
 }
 // fill table and setup edit/delete functionality for each row
 function todoTableSetup() {
+  // get body of todo table
+  let tableBody = document.getElementById("todoTable");
+
+  // fill table from fetched todo data
+  for (const [i, todo] of sampleData.entries()) {
+    let tr = document.createElement('TR');
+    tableBody.appendChild(tr);
+
+    // create # cell
+    let th = document.createElement('TH')
+    th.appendChild(document.createTextNode(i+1))
+    th.scope = "row"
+    tr.appendChild(th)
+    
+    // create property cells
+    for (let [key, value] of Object.entries(todo)) {
+      let td = document.createElement('TD')
+      
+      if (key === "dueDate")
+        value = new Date(value * 1000).toLocaleDateString()
+      
+      td.appendChild(document.createTextNode(value))
+      tr.appendChild(td)
+    }
+
+    // create edit/delete cell
+    tr.innerHTML +=`<td>
+                      <div id="deleteTask${i}" class="table-btn">
+                        <i class="bi bi-x"></i>
+                      </div>
+                      <div id="editTask${i}" class="table-btn">
+                        <i class="bi bi-pencil-square"></i>
+                      </div>
+                    </td>`
+
+    // set edit/delete click functionality
+    $(`#deleteTask${i}`).on('click', function() {
+    })
+
+    $(`#editTask${i}`).on('click', function() {
+      window.localStorage.setItem("editItem", JSON.stringify(todo))
+      window.location.replace("/editTODO.html");
+    })
+  } 
 }
 
 $(function() {
