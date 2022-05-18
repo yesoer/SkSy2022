9816d11f9b04866be836d023281a03eaa76e1a94
yesoer <luisenglaender@web.de>
2022-05-18 22:56:50 +0200

    setup route stubs
    Co-Authored-By: Yassien <32311529+Informatik-Ignaz@users.noreply.github.com>

--- backend/main.go
+++ backend/main.go
@@ -5,11 +5,50 @@ import (
 )
 
 func main() {
+	r := initRouter()
+
+	// listen and serve on default port 8080
+	r.Run()
+}
+
+// initializes the gin router with the necessary middleware
+func initRouter() *gin.Engine {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/ping", pingHandler())
+
+	r.GET("/todo", getTodoHandler())
+	r.PUT("/todo", putTodoHandler())
+	r.POST("/todo", postTodoHandler())
+	r.DELETE("/todo", deleteTodoHandler())
+	return r
+}
+
+// pingHandler returns a simple string response
+func pingHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	}
+}
+
+func getTodoHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// TODO : get todo from db
+	}
+}
+func putTodoHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// TODO : update todo in db
+	}
+}
+func postTodoHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// TODO : create todo in db
+	}
+}
+func deleteTodoHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// TODO : delete todo from db
+	}
 }
